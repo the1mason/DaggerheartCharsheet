e3ca318be60f696d004af0caace3bf0b36a9af88
the1mason <mail@the1mason.com>
2025-10-24 03:30:30 +0500

    added cruicial comments

--- DaggerheartCharsheet.Core/Models/Sheets/Sections/BasicInfoSection.cs
+++ DaggerheartCharsheet.Core/Models/Sheets/Sections/BasicInfoSection.cs
@@ -4,7 +4,11 @@ namespace DaggerheartCharsheet.Core.Models.Sheets.Sections;
 
 public sealed class BasicInfoSection : VersionedSection
 {
+    // each ancestry is version-dependent and thus should have version attached.
+    // A sheet may have ancestry from a different game version and that should be localized properly.
+    // I will bring back ancestry card class later
     public AncestryType[] Ancestries { get; set; } = [];
+    // similar to ancestries, classes are version-dependent
     public SubclassType? Subclass { get; set; }
     public int Level { get; set; }
 }
