688d7dbca4fda7ea8688b82cac2d1af9b71eace1
the1mason <mail@the1mason.com>
2025-10-26 01:13:34 +0500

    added important comment. will fix later.

--- DaggerheartCharsheet.Core/Models/Sheets/Sections/CardsSection.cs
+++ DaggerheartCharsheet.Core/Models/Sheets/Sections/CardsSection.cs
@@ -5,6 +5,10 @@ namespace DaggerheartCharsheet.Core.Models.Sheets.Sections;
 
 public sealed class CardsSection : VersionedSection
 {
+    // here the type is wrong
+    // realistically it should be just a pair of card type and version type and then the
+    // display logic should retreive the correct card based on version and type
+    // maybe make a generic entity for that exaxt scenario as it is used often
     public Dictionary<GameVersionType, DomainCard[]> DomainCards { get; set; } = new();
     public Dictionary<GameVersionType, SubclassCard[]> SubclassCards { get; set; } = new();
 }
